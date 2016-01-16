8d0fceea830a355c36e7c296be11e03019dccdb2
Barnett Trzcinski <btrzcinski@gmail.com>
2016-01-16 15:24:19 -0800

    Added plumbing for adding instruction support, and added NOP to start

--- cpp/be/be.cpp
+++ cpp/be/be.cpp
@@ -2,21 +2,66 @@
 
 #include <iostream>
 
+#define CALL_MEMBER_FN(object,ptrToMember)  ((object)->*(ptrToMember))
+
 using namespace Backend;
 
 using std::uint16_t;
 
-VirtualMachine::VirtualMachine()
+VirtualMachine::VirtualMachine() :
+    expectation(Expectation::Instruction),
+    instruction(nullptr)
 {
-	
+    add_instruction(21, 0, &VirtualMachine::nop_fn);
 }
 
 VirtualMachine::~VirtualMachine()
 {
-	
 }
 
 void VirtualMachine::next_word(uint16_t word)
 {
-	std::cout << word << std::endl;
+    if (expectation == Expectation::Instruction)
+    {
+        auto mappedInstruction = opcodeInstructionMap.find(word);
+        if (mappedInstruction == opcodeInstructionMap.end())
+        {
+            std::cerr << "Warning: unknown opcode " << word << std::endl;
+            return;
+        }
+
+        instruction = &(mappedInstruction->second);
+        arguments.clear();
+
+        if (instruction->numArguments > 0)
+        {
+            expectation = Expectation::Argument;
+        }
+        else
+        {
+            CALL_MEMBER_FN(this, instruction->fn)();
+        }
+    }
+    else if (expectation == Expectation::Argument)
+    {
+        arguments.push_back(word);
+        if (arguments.size() == instruction->numArguments)
+        {
+            CALL_MEMBER_FN(this, instruction->fn)();
+            expectation = Expectation::Instruction;
+        }
+    }
+}
+
+void VirtualMachine::add_instruction(std::uint16_t opcode, int numArguments, InstructionFn fn)
+{
+    opcodeInstructionMap.emplace(opcode, Instruction(opcode, numArguments, fn));
+}
+
+void VirtualMachine::nop_fn()
+{
+    // NOP = opcode 21
+    // Do nothing.
+ 
+    std::cout << "NOP" << std::endl;   
 }
--- cpp/be/be.h
+++ cpp/be/be.h
@@ -1,4 +1,7 @@
 #include <cstdint>
+#include <functional>
+#include <unordered_map>
+#include <vector>
 
 namespace Backend
 {
@@ -9,5 +12,38 @@ namespace Backend
 		virtual ~VirtualMachine();
 		
 		void next_word(std::uint16_t word);
+
+    private:
+        enum class Expectation
+        {
+            Instruction,
+            Argument
+        };
+
+        typedef void (VirtualMachine::*InstructionFn)(void);
+
+        struct Instruction
+        {
+            Instruction(std::uint16_t opcode, int numArguments, InstructionFn fn) :
+                opcode(opcode),
+                numArguments(numArguments),
+                fn(fn)
+            {
+            }
+
+            std::uint16_t opcode;
+            int numArguments;
+            InstructionFn fn;
+        };
+
+        void add_instruction(std::uint16_t opcode, int numArguments, InstructionFn fn);
+
+        void nop_fn();
+
+        Expectation expectation;
+        Instruction const* instruction;
+        std::vector<std::uint16_t> arguments;
+
+        std::unordered_map<std::uint16_t, Instruction> opcodeInstructionMap;
 	};
 }
